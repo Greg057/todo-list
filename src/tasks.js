cd6202d74bc405db52e9c532b3ae0d34dd326b21
Greg057 <88341092+Greg057@users.noreply.github.com>
2024-01-15 17:40:07 +0100

    Fix Add task logic and UI

--- src/tasks.js
+++ src/tasks.js
@@ -8,7 +8,6 @@ class Task {
     }
 
     createUI () {
-        console.log(this.text)
         const tasks = document.querySelector("#tasks")
         const taskItem = document.createElement("div")
         taskItem.innerHTML = `<div class="task-item">
@@ -20,17 +19,29 @@ class Task {
                                 <span class="material-symbols-outlined">delete</span>
                             </div>`
         tasks.appendChild(taskItem)
+
+        const childToRemove = document.querySelector(".task-field")
+        tasks.removeChild(childToRemove)
+
+        const addTaskBtn = document.querySelector("#add-task")
+        addTaskBtn.style.visibility = "visible"
+
         return tasks
+    }
+
+    removeTask () {
+
     }
 
 
 }
 
-function addTask (event, text, priority, date) {
-    form.reset();
+function addTask (event) {
     event.preventDefault();
-    console.log(text)
-    const task = new Task(text, priority, date)
+    const textForm = document.getElementById("text-field").value
+    const priorityForm = document.querySelector("#priority").value
+    const dateForm = document.querySelector("#date").value
+    const task = new Task(textForm, priorityForm, dateForm)
     task.createUI()
 
 }
@@ -38,30 +49,24 @@ function addTask (event, text, priority, date) {
 function openTaskInput() {
     const tasks = document.querySelector("#tasks")
     const inputField = document.createElement("div")
-    inputField.innerHTML = `<div class="task-field">
-                                    <form>
-                                        <input type="text" id="text-field" name="text-field" placeholder="What is your task?" required>
-                                        <select id="priority">
-                                            <option value="low">Priority low</option>
-                                            <option value="medium">Priority medium</option>
-                                            <option value="high">Priority high</option>
-                                        </select>
-                                        <input type="date" id="date"></input>
-                                        <button type="submit" id="add-task-form">Add task</button>
-                                    </form>
-                                </div>`
+    inputField.classList.add("task-field")
+    inputField.innerHTML = `<form>
+                                <input type="text" id="text-field" name="text-field" placeholder="What is your task?" required>
+                                <select id="priority">
+                                    <option value="low">Priority low</option>
+                                    <option value="medium">Priority medium</option>
+                                    <option value="high">Priority high</option>
+                                </select>
+                                <input type="date" id="date"></input>
+                                <button type="submit" id="add-task-form">Add task</button>
+                            </form>`
     tasks.appendChild(inputField)
 
     const addTaskBtn = document.querySelector("#add-task")
     addTaskBtn.style.visibility = "hidden"
 
-    const textForm = document.getElementById("text-field")
-    console.log(textForm.value)
-    const priorityForm = document.querySelector("#priority").value
-    const dateForm = document.querySelector("#date").value
-
     const addTaskFormBtn = document.querySelector("#add-task-form")
-    addTaskFormBtn.addEventListener("click", (event) => addTask(event, textForm, priorityForm, dateForm))
+    addTaskFormBtn.addEventListener("click", (event) => addTask(event))
 }
 
 export { openTaskInput }
