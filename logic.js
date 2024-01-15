1db8fe567a2ad789eafbb69db14204c1e324ec73
Greg057 <88341092+Greg057@users.noreply.github.com>
2024-01-15 09:22:44 +0100

    Webpack setup

---
+++ logic.js
@@ -0,0 +1,17 @@
+class Todo {
+    constructor(title, description, dueDate, priority) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    }
+
+    addToProject() {
+
+    }
+
+    removeFromProject() {
+
+    }
+}
+
